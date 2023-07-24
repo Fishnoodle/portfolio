bb46f7d658024b386eaa9e527018e219537fe6f1
Fishnoodle <shivar.vishal.pillay@gmail.com>
2023-07-24 01:06:58 -0700

    testing change

--- components/Navbar.js
+++ components/Navbar.js
@@ -49,7 +49,7 @@ const Navbar = () => {
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/'>
                     <Image
-                        src="/../public/logo.png"
+                        src="../public/logo.png"
                         alt="/"
                         width='50'
                         height='50'
