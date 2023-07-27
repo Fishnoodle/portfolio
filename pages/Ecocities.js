1ff88f5b5bb3bd24566db3020849a60ca249efd5
Fishnoodle <shivar.vishal.pillay@gmail.com>
2023-07-26 23:04:31 -0700

    Remove Button - Ecocities

--- pages/Ecocities.js
+++ pages/Ecocities.js
@@ -34,7 +34,6 @@ const project2 = () => {
                     <button className='px-8 py-2 mt-4 mr-8'>OverLeaf</button>
                 </Link>
                 
-                <button className='px-8 py-2 mt-4'>Code</button>
             </div>
             <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
                 <div className='p-2'>
