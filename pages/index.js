7ef13323d6549341f5382915e17d7f0a17e8215e
Fishnoodle <shivar.vishal.pillay@gmail.com>
2023-07-31 17:00:58 -0700

    fixed mobile-view chrome; configured meta

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Vishal Pillay | Portfolio</title>
-        <meta name="This is my portfolio" content="Generated with next.js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
         <link rel="icon" href="/logo.png" />
       </Head>
       <Main />
